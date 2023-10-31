92fc1dc880e9492b9dbf22e09c13cf6dcd6fd62d
APbigstar1 <anthony.pen1115@gmail.com>
2023-10-31 10:54:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const serverless = require("serverless-http");
 
 // Rate Limiter
 const rateLimiter = require("./src/middleware/AuthMiddleware");
@@ -32,12 +31,15 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+const PORT = process.env.PORT || 9000;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {})
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
   .catch((error) => console.log(`${error} did not connect.`));
 
 app.use("/api/auth", authRoutes);
@@ -45,5 +47,3 @@ app.use("/api/client", clientRoutes);
 app.use("/api/general", generalRoutes);
 app.use("/api/management", managementRoutes);
 app.use("/api/sales", salesRoutes);
-
-export const handler = serverless(app);
